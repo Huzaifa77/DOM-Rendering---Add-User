249b60cd22bfda9db89aafb3138d01a3563f6149
Mohammed Huzaifa <38620666+Huzaifa77@users.noreply.github.com>
2021-04-21 22:39:08 +0530

    Second day commit

--- src/index.js
+++ src/index.js
@@ -4,18 +4,28 @@ const api = `https://randomuser.me/api`;
 const addUser = document.getElementById("userAdd-btn");
 const mainApp = document.getElementById("app");
 
+const appState = [];
+
 addUser.addEventListener("click", async () => {
   const userData = await fetch(api, {
     method: "GET"
   });
 
   const userDataJson = await userData.json(); //it is also asynchronous operation and a aSYNC operation
-
   const user = userDataJson.results[0];
-  const userEl = document.createElement("div");
-  userEl.innerHTML = `<div>
-    ${user.name.title} ${user.name.first} ${user.name.last}
-  </div>`;
 
-  mainApp.appendChild(userEl);
+  appState.push(user);
+  console.log(appState);
+  domRender(appState);
 });
+
+const domRender = (stateArr) => {
+  stateArr.forEach((userObj) => {
+    const userEl = document.createElement("div");
+    userEl.innerHTML = `<div>
+    ${userObj.name.title} ${userObj.name.first} ${userObj.name.last}
+    </div>`;
+
+    mainApp.appendChild(userEl);
+  });
+};
---
+++ src/index2.js
@@ -0,0 +1,10 @@
+var teacher = "huzaifa";
+
+function other() {
+  let teacher = "pri";
+
+  function ask(q) {
+    console.log(teacher, q);
+  }
+  console.log(q);
+}
